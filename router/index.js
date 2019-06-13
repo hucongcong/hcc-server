5e781eaa5c081e2afb9e0079fd5e1ac07772a947
hucongcong <flycc258@163.com>
2019-06-13 18:24:07 +0800

    文件列表展示功能完成

--- router/index.js
+++ router/index.js
@@ -4,11 +4,13 @@ const promisify = require('util').promisify
 const stat = promisify(fs.stat)
 const readdir = promisify(fs.readdir)
 const mime = require('mime')
+const template = require('art-template')
 
 const { root } = require('../config')
 // 处理路由
 async function router(req, res) {
   const filePath = path.join(root, req.url)
+  console.log(filePath)
   // 读取文件状态，判断是文件还是文件夹
   try {
     const stats = await stat(filePath)
@@ -21,18 +23,31 @@ async function router(req, res) {
       }
       res.setHeader('content-type', contentType)
       // 以流的形式返回给客户端
-      console.log(filePath)
       fs.createReadStream(filePath).pipe(res)
     } else if (stats.isDirectory()) {
       // 如果是文件夹
-      const result = await readdir(filePath)
-      res.end(result.join('-'))
+      let files = await readdir(filePath)
+      const tmlPath = path.join(__dirname, '..', 'views', 'index.html')
+      files = files.map(item => {
+        return {
+          name: item,
+          dir: path.join('/' + path.relative(root, filePath), item)
+        }
+      })
+      const data = {
+        title: path.basename(filePath),
+        files
+      }
+      console.log(root, filePath, data.dir)
+      const html = template(tmlPath, data)
+      res.setHeader('content-type', 'text/html')
+      res.end(html)
     }
   } catch (e) {
     // 处理404
+    console.log(e)
     render404(res)
   }
-  res.end(filePath)
 }
 
 // 响应404
